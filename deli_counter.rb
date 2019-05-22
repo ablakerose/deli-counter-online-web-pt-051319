5eb8a329bcbeba2ea1d2224f4efd67417dfd128f
Blake Rose <bdecktor@gmail.com>
2019-05-22 23:51:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(katz_deli)
   line_positions = []
-  if katz_deli.empty? 
+  if katz_deli.empty?
     puts "The line is currently empty."
-  else 
-    number = 1 
+  else
+    number = 1
     katz_deli.each do |customer|
       line_positions << "#{number}. #{customer}"
     number += 1
