f1d79e7e0a001458ee529fd902ab2ab7d7125224
Blake Rose <bdecktor@gmail.com>
2019-05-23 00:02:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,3 @@
-def line(katz_deli)
-  line_positions = []
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    number = 1
-    katz_deli.each do |customer|
-      line_positions << "#{number}. #{customer}"
-    number += 1
-  end
-end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
